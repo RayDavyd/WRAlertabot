36c0aae6a92beb9f5e50b4968a5d42829f76167b
RayDavyd <raydavyd8@gmail.com>
2026-06-08 16:13:25 -0300

    feat: filtra manutencoes apenas para cidades permitidas

--- bot.py
+++ bot.py
@@ -13,11 +13,14 @@ load_dotenv()
 
 TOKEN   = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-
 SHEET_ID = "1vERB1h8xCrtXWSqQZO0z5pDOmicKj9XSvgOn4xto_Ls"
 GID      = "688165482"
 URL_CSV  = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv&gid={GID}"
-
+CIDADES_PERMITIDAS = {
+    "SOUSA", "POMBAL", "VIEIRÓPOLIS", "COREMAS", "SÃO BENTINHO",
+    "SÃO DOMINGOS", "MALTA", "CONDADO", "SÃO FRANCISCO",
+    "SANTA CRUZ", "CAJAZEIRINHAS", "LASTRO"
+}
 
 def ler_planilha():
     df = pd.read_csv(URL_CSV)
@@ -26,19 +29,15 @@ def ler_planilha():
     return df
 
 
-#if __name__ == "__main__":
- #   ler_planilha()
-
-#Filtro de manutenções para amanhã
-
-from datetime import datetime, timedelta
 
 def ler_manutencoes_amanha():
     df = pd.read_csv(URL_CSV)
     df["DATA PROGRAMADA"] = pd.to_datetime(df["DATA PROGRAMADA"], dayfirst=True, errors="coerce")
     df = df.dropna(subset=["DATA PROGRAMADA"])
-    amanha = (datetime.now() + timedelta(days=1)).date() 
-    return df[df["DATA PROGRAMADA"].dt.date == amanha]
+    amanha = (datetime.now() + timedelta(days=1)).date()
+    df = df[df["DATA PROGRAMADA"].dt.date == amanha]
+    df = df[df["LOCAL"].str.strip().str.upper().isin(CIDADES_PERMITIDAS)]
+    return df
 
 #Controle de duplicatas
 
